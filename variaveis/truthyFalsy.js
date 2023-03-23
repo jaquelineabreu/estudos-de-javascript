04aae3170676fc17624eeb2521ea33955d3a5609
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2023-03-22 21:15:02 -0300

    estudos de booleanos, null e undefined

---
+++ variaveis/truthyFalsy.js
@@ -0,0 +1,26 @@
+//boolean
+let usuarioLogado = true
+let contaPaga = false
+
+//truthy ou falsy
+console.log(0 == false)
+console.log('' == false)
+console.log(1 == true)
+
+//undefined
+let minhaVar
+
+//null
+let minhaNull = null
+
+console.log(minhaVar)
+console.log(minhaNull)
+
+let numero = 3
+let nome = 'Jaque'
+
+console.log(typeof numero)
+console.log(typeof nome)
+
+console.log(typeof minhaVar)
+console.log(typeof minhaNull)
