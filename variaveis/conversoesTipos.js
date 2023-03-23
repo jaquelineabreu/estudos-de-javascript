e7c85c68386f24f13f34c7e09687880ad270f2e2
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2023-03-22 21:31:17 -0300

    conversões de tipos

---
+++ variaveis/conversoesTipos.js
@@ -0,0 +1,14 @@
+//Conversão de tipos
+//Booleanos
+
+//conversao implicita
+let numero = 36
+let numeroString = Number('36')
+
+console.log(numero == numeroString)
+
+console.log(numero + numeroString)
+
+//conversão explicita
+
+console.log(numero + numeroString)
