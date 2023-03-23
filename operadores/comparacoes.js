2d64d6f25999cdae324f7802172fe4eb0e1fe0be
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2023-03-23 19:36:05 -0300

    operadores

---
+++ operadores/comparacoes.js
@@ -0,0 +1,19 @@
+// == comparação implicita
+
+let numero = 5
+let texto = '5'
+
+console.log(numero == texto)
+
+// == comparação
+console.log(numero === texto)
+
+//typeof
+console.log(typeof numero)
+console.log(typeof texto)
+
+//operador ternario
+let idadeMinina = 18
+let idadeCliente = 19
+
+console.log(idadeCliente >= idadeMinina ? 'Cerveja' : 'Suco')
