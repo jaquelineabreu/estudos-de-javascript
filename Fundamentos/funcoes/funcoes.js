2688ce7064bc1ef280973e23c5ce2a28c09bd6ee
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2023-03-23 23:25:54 -0300

    func sem parametro

---
+++ Fundamentos/funcoes/funcoes.js
@@ -0,0 +1,14 @@
+function imprimeTexto(texto) {
+  console.log(texto)
+}
+
+imprimeTexto('Oi')
+
+//sem parametro
+function soma() {
+  return 2 + 2
+}
+
+//console.log(soma())
+
+imprimeTexto(soma())
