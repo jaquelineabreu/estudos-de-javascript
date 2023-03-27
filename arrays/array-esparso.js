46dffe4a68c9fb7ec374afc528479f980bf5b96b
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2023-03-27 17:06:26 -0300

    array esparso

---
+++ arrays/array-esparso.js
@@ -0,0 +1,15 @@
+let arrayVazia = []
+
+console.log(arrayVazia)
+console.log(arrayVazia.length)
+console.log(arrayVazia[0])
+
+arrayVazia = [, , ,]
+console.log(arrayVazia.length)
+console.log(arrayVazia[0])
+console.log(arrayVazia[1])
+console.log(arrayVazia[2])
+
+arrayVazia.push(50)
+console.log(arrayVazia)
+console.log(arrayVazia.length)
